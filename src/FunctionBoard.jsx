8e98356579932025051a5766994dcec3fe967c0d
AshishBhardwaj1 <fireash70@gmail.com>
2024-01-19 17:13:57 +0530

    added css

--- src/FunctionBoard.jsx
+++ src/FunctionBoard.jsx
@@ -49,13 +49,13 @@ const initialMove = Array(9).fill(null);
     const isBoardFull = squares.every((square)=> square!== null)
     let status;
     if(winner) {
-        status = "winner" +winner
+        status = "winner " +winner
     } else  if (isBoardFull){
-        status ="the game is tied"
+        status ="the game is Draw"
     }
     
     else {
-        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+        status = ' player: ' + (xIsNext ? '1 X' : '2 O');
         
         
     }
@@ -66,25 +66,33 @@ const initialMove = Array(9).fill(null);
     return (
         
 
-    <div className='tic-tac-toe'>
-     <div>{status} </div>
+    <div className='tic-tac-toe'> 
+                
+                 <div><h1>TIC-TAC-TOE</h1></div>
+                         <div className='status'>{status} </div>
 
     <div className='board-row'>
     <Square val= {squares[0]} onSquareClick={()=>handleClick(0)}/>
+ 
     <Square  val= {squares[1]} onSquareClick={()=>handleClick(1)}/>
+   
     <Square  val= {squares[2]} onSquareClick={()=>handleClick(2)}/>
+
+    </div>
+
     <div className='board-row'>
     <Square  val= {squares[3]} onSquareClick={()=>handleClick(3)}/>
     <Square  val= {squares[4]} onSquareClick={()=>handleClick(4)}/>
     <Square  val= {squares[5]} onSquareClick={()=>handleClick(5)}/>
     </div>
+
     <div className='board-row'>
     <Square  val= {squares[6]} onSquareClick={()=>handleClick(6)}/>
     <Square  val= {squares[7]} onSquareClick={()=>handleClick(7)}/>
-    <Square  val= {squares[8]} onSquareClick={()=>handleClick(8)}/>
+    
+    <Square  val= {squares[8]} onSquareClick={()=>handleClick(8)} />
     </div>
-    </div>
-    <button style={{height:"30px",width:'100px',fontSize:"15px"}} onClick={reset}> play again </button>
+    <button style={{backgroundColor:"purple",height:"30px",width:'100px',fontSize:"15px",borderRadius:"20px",margin:"20px",color:"white"}} onClick={reset}> play again </button>
   
  
     </div>
